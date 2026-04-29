bccec77a7f3b0581a71e3fe9942d01f384832904
Aditya Gupta <adityagupta1122006@gmail.com>
2026-04-30 02:40:43 +0530

    Cleaned repository

--- MY_robot/build/my_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aditya4430/ROS_PROJECT/MY_robot/src/my_robot
-# Build directory: /home/aditya4430/ROS_PROJECT/MY_robot/build/my_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
